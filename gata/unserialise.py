cf7ba0f4cdf6ceb0d2d3d2472e9e4cfdf71a6725
Dawid Kraczkowski <dawid.kraczkowski@gmail.com>
2020-01-07 23:16:28 +0100

    Refactor unserialise function

--- gata/unserialise.py
+++ gata/unserialise.py
@@ -4,54 +4,79 @@ from datetime import time
 from enum import Enum
 from inspect import isclass
 from typing import Any
+from typing import Type
+from typing import TypeVar
 
 from .format import Format
 from .utils.is_dataclass_type import is_dataclass_type
 
 ARRAY_TYPES = (list, set)
 
+T = TypeVar('T')
+
+
+def _unserialise_dataclass(value: dict, target_type: Type[T]) -> T:
+    if not isinstance(value, dict):
+        raise ValueError(
+            f"Cannot unserialise value of type {type(value)} to {target_type}."
+        )
+    return target_type.unserialise(value)
+
+
+def _unserialise_list(value, target_type, meta: dict) -> list:
+    (arg_type,) = target_type.__args__
+    if not arg_type:
+        raise ValueError(
+            "Cannot unserialise generic lists/sets. Please List/Set subtype."
+        )
+
+    return [
+        unserialise(item, arg_type, meta["items"] if "items" in meta else {})
+        for item in value
+    ]
+
+
+def _unserialise_set(value, target_type, meta: dict) -> set:
+    (arg_type,) = target_type.__args__
+    if not arg_type:
+        raise ValueError(
+            "Cannot unserialise generic lists/sets. Please List/Set subtype."
+        )
+
+    return set(
+        unserialise(item, arg_type, meta["items"] if "items" in meta else {})
+        for item in value
+    )
+
+
+def _unserialise_string(value, target_type, meta) -> Any:
+    if "format" in meta:
+        formatter = Format.get_formatter(meta["format"])
+        return formatter.hydrate(value)
+
+    return value
+
 
 def unserialise(value: Any, target_type, meta: dict = {}) -> Any:
-
     # Enums
     if isclass(target_type) and issubclass(target_type, Enum):
         return target_type(value)
 
     # Dataclass
     if is_dataclass_type(target_type):
-        if not isinstance(value, dict):
-            raise ValueError(
-                f"Cannot unserialise value of type {type(value)} to {target_type}."
-            )
-        return target_type.unserialise(value)
+        return _unserialise_dataclass(value, target_type)
 
     # List and sets
     origin_type = getattr(target_type, "__origin__", None)
     if origin_type and origin_type in ARRAY_TYPES:
-        (arg_type,) = target_type.__args__
-        if not arg_type:
-            raise ValueError(
-                "Cannot unserialise generic lists/sets. Please List/Set subtype."
-            )
-
         if origin_type == list:
-            return [
-                unserialise(item, arg_type, meta["items"] if "items" in meta else {})
-                for item in value
-            ]
-
-        return (
-            unserialise(item, arg_type, meta["items"] if "items" in meta else {})
-            for item in value
-        )
+            return _unserialise_list(value, target_type, meta)
+        else:
+            return _unserialise_set(value, target_type, meta)
 
     # Strings
     if target_type is str:
-        if "format" in meta:
-            formatter = Format.get_formatter(meta["format"])
-            return formatter.hydrate(value)
-
-        return value
+        return _unserialise_string(value, target_type, meta)
 
     # Datetimes
     if target_type is datetime:
@@ -70,3 +95,6 @@ def unserialise(value: Any, target_type, meta: dict = {}) -> Any:
         return target_type(value)
 
     return value
+
+
+__all__ = ["unserialise"]
